4791d770d0c010eaa060e10ca1dee04e7d7595fc
Ruediger Gad <r.c.g@gmx.de>
2012-07-14 13:23:16 +0200

    Update version info. We are now using Clojure 1.4.0 and cljAcmeUtils 1.5.0.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-;(defproject cljNetPcap "1.4.2"
-(defproject cljNetPcap "1.5.0-SNAPSHOT"
+;(defproject cljNetPcap "1.5.0"
+(defproject cljNetPcap "1.6.0-SNAPSHOT"
   :description "cljNetPcap is a wrapper/adapter/facade (No matter how you wanna call it.) 
                 around jNetPcap that enables and eases packet capturing with Clojure."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.cli "0.2.1"]
-                 [cljAcmeUtils "1.5.0"]
+                 [cljAcmeUtils "1.6.0"]
                  [jnetpcap "1.4.r1300-2"]]
   :main cljNetPcap.main
   :java-source-path "src"
