c8a50526b1f0be0499b6415af817462cef77cef9
Ruediger Gad <r.c.g@gmx.de>
2012-11-13 09:51:08 +0100

    Remove tools.timer as it seems to be broken.

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.cli "0.2.2"]
                  [cljAcmeUtils "1.7.0"]
-                 [jnetpcap "1.4.r1390-1a"]
-                 [ruiyun/tools.timer "1.0.0-SNAPSHOT"]]
+                 [jnetpcap "1.4.r1390-1a"]]
   :main cljNetPcap.main
   :java-source-path "src"
   :omit-source true)
