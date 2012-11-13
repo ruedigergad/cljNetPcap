e131818c210183cefdf63ff75d766a4b7444318f
Ruediger Gad <r.c.g@gmx.de>
2012-11-13 09:42:02 +0100

    Update tools.cli.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :description "cljNetPcap is a wrapper/adapter/facade (No matter how you wanna call it.) 
                 around jNetPcap that enables and eases packet capturing with Clojure."
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/tools.cli "0.2.1"]
+                 [org.clojure/tools.cli "0.2.2"]
                  [cljAcmeUtils "1.7.0"]
                  [jnetpcap "1.4.r1390-1a"]
                  [ruiyun/tools.timer "1.0.0-SNAPSHOT"]]
